4f86c9f3a65ea94c759594b66fe6abf02f609648
jcalcivarg <jcalcivar@hotmail.com>
2024-08-05 16:19:51 -0500

    prrograma test8

--- programa.py
+++ programa.py
@@ -87,29 +87,31 @@
 #     # Se solicita al usuario que ingrese el segundo número
 #     print("La suma es:", suma(num1, num2))  # Se imprime la suma de los dos números ingresados
 
-# 7 Modifica la función que calcula el área del círculo para que reciba el radio como parámetro.
-import math
+# # 7 Modifica la función que calcula el área del círculo para que reciba el radio como parámetro.
+# import math
 
-def area_circulo(radio):
-    area = math.pi * radio ** 2  # Se calcula el área del círculo utilizando la fórmula matemática
-    return area;
-    # Se devuelve el área calculada
-
-if __name__ == "__main__":
-    radio = float(input("Ingrese el radio del círculo: "))  # Se solicita al usuario que ingrese el radio del círculo
-    print("El área del círculo es:", area_circulo(radio))  # Se imprime el área calculada del círculo
-    
-
-# # 8 Diseña un programa que convierta grados Celsius a Fahrenheit y viceversa, utilizando funciones para realizar los cálculos.
-# def celsius_a_fahrenheit(celsius):
-#     return (celsius * 9/5) + 32  # Se aplica la fórmula de conversión de Celsius a Fahrenheit
-
-# def fahrenheit_a_celsius(fahrenheit):
-#     # Se aplica la fórmula de conversión de Fahrenheit a Celsius
+# def area_circulo(radio):
+#     area = math.pi * radio ** 2  # Se calcula el área del círculo utilizando la fórmula matemática
+#     return area;
+#     # Se devuelve el área calculada
 
 # if __name__ == "__main__":
-#     celsius = float(input("Ingrese la temperatura en grados Celsius: "))  # Se solicita al usuario que ingrese la temperatura en grados Celsius
-#     print("Temperatura en Fahrenheit:", celsius_a_fahrenheit(celsius))  # Se imprime la temperatura convertida a grados Fahrenheit
-#     # Se solicita al usuario que ingrese la temperatura en grados Fahrenheit
-#     print("Temperatura en Celsius:", fahrenheit_a_celsius(fahrenheit))  # Se imprime la temperatura convertida a grados Celsius
+#     radio = float(input("Ingrese el radio del círculo: "))  # Se solicita al usuario que ingrese el radio del círculo
+#     print("El área del círculo es:", area_circulo(radio))  # Se imprime el área calculada del círculo
+    
+
+# 8 Diseña un programa que convierta grados Celsius a Fahrenheit y viceversa, utilizando funciones para realizar los cálculos.
+def celsius_a_fahrenheit(celsius):
+    return (celsius * 9/5) + 32  # Se aplica la fórmula de conversión de Celsius a Fahrenheit
+
+def fahrenheit_a_celsius(fahrenheit):
+    return (fahrenheit -32 ) *  5/9 
+    # Se aplica la fórmula de conversión de Fahrenheit a Celsius
+
+if __name__ == "__main__":
+    celsius = float(input("Ingrese la temperatura en grados Celsius: "))  # Se solicita al usuario que ingrese la temperatura en grados Celsius
+    print("Temperatura en Fahrenheit:", celsius_a_fahrenheit(celsius))  # Se imprime la temperatura convertida a grados Fahrenheit
+    # Se solicita al usuario que ingrese la temperatura en grados Fahrenheit
+    fahrenheit = float(input("Ingrese la temperatura en grados Celsius: "))  # Se solicita al usuario que ingrese la temperatura en grados Celsius
+    print("Temperatura en Celsius:", fahrenheit_a_celsius(fahrenheit))  # Se imprime la temperatura convertida a grados Celsius
 
